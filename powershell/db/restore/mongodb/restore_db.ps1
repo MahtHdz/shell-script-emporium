29357bfec34e4a722d6e0bae64757a39a7942fde
MahtHdz <miguel.hztapia@outlook.com>
2024-07-15 07:54:10 -0600

    refactor: :fire: remove code
    Old database powershell scripts were deleted.

--- powershell/db/restore/mongodb/restore_db.ps1
+++
@@ -1,5 +0,0 @@
-$dbName = Read-Host -Prompt "Enter the db name"
-$dbURI = Read-Host -Prompt "Enter the db uri"
-$backupPath = Read-Host -Prompt "Enter the backup db path"
-& mongorestore --uri=$dbURI --gzip --db $dbName $backupPath | Tee-Object -Variable dumpOutput
-Write-Host "Backup restored sucessfully!" -ForegroundColor Green
